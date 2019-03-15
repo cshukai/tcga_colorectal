957aead0d65b514f076280b132af5cc842fbadd4
cshukai <cshukai@gmail.com>
2019-03-14 21:13:38 -0500

    remove noise

--- regimen.r
+++ regimen.r
@@ -3,8 +3,17 @@ library(plotly)
 library(ggridges)
 
 load("regimen.RData")
+
+
 cycles=NULL
 for(i in 1:length(result)){
-    cycles=c(cycles,length(grep("number",names(result[[i]]))))
-    
+    if(length(grep("number",names(result[[i]])))==0){
+        print(result[[i]])
+    }
+    else{
+        cycles=c(cycles,length(grep("number",names(result[[i]]))))
+    }
 }
+
+table(cycles)
+
