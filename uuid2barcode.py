239813c8c035303a53098025434e058503a393c9
Shu-Kai Chang <cshukai@gmail.com>
2019-07-10 23:17:40 -0500

    not sure how to use exception handle

--- uuid2barcode.py
+++ uuid2barcode.py
@@ -1,16 +1,25 @@
+[shchang@lewis4-r630-login-node676 wholeslides]$ more uuid2barcode.py 
 #python2
-import requests
+#import requests
 import json
 import os
 import pandas
+import grequests
+
+    
 
 uuids=os.listdir('coad')
 file_endpt = 'https://api.gdc.cancer.gov/legacy/files/'
 df=pandas.DataFrame(columns=['uuid', 'barcode'])
+
+urls=[]
 for i,uuid in enumerate(uuids) :
-        response = requests.get(file_endpt + uuid)
-        x=response.json()
-        y=x['data']['file_name']
-        barcode="-".join(y.split("-")[0:3])
-        df.loc[i]=uuid+barcode
+    urls.append(file_endpt + uuid)
+
+rs = (grequests.get(u) for u in urls)
+grequests.map(rs)
+#        x=response.json()
+#        y=x['data']['file_name']
+#        barcode="-".join(y.split("-")[0:3])
+#        df.loc[i]=uuid+barcode
 df.to_csv(path="coad.barcode.csv")
